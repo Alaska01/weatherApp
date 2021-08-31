3dbf3096341dcb51a2f6785ddf107ff0dcc0e60c
Aye Daniel A <aadaniel108@gmail.com>
2021-08-31 15:47:14 +0100

    fix style errors and remove comment

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,3 @@
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const path = require('path');
-
-// module.exports = {
-//   entry: {
-//     index: path.resolve(__dirname, './src/index.js'),
-//   },
-//   output: {
-//     path: path.resolve(__dirname, './dist'),
-//     filename: 'main.js',
-//   },
-//   plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' }), new MiniCssExtractPlugin({ filename: './src/style.css' })],
-//   module: {
-//     rules: [
-//       {
-//         test: /\.css$/,
-//         use: ['style-loader', 'css-loader'],
-//       },
-//       {
-//         test: /\.scss$/,
-//         use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader', 'sass-loader'],
-//       },
-//       {
-//         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-//         type: 'asset/resource',
-//       },
-//       {
-//         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-//         type: 'asset/resource',
-//       },
-//     ],
-//   },
-// };
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
